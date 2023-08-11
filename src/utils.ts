837964d96d036482f69a4b18f6bad0648b245d00
Fabio Spampinato <spampinabio@gmail.com>
2023-08-11 16:25:57 +0100

    Updated rendering logic, to fix flickering

--- src/utils.ts
+++ src/utils.ts
@@ -13,9 +13,9 @@ const writeLine = ( line: string ): void => {
 
   if ( process ) {
 
-    process.stdout?.clearLine?.( 0 );
     process.stdout?.cursorTo?.( 0 );
     process.stdout?.write?.( line );
+    process.stdout?.clearLine?.( 1 );
 
   } else {
 
